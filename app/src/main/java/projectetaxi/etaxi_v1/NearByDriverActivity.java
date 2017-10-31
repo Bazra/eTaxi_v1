6a9e3792284be7aaeef401eb27de3cea86a31900
Jharana Gurung <jharna.915@gmail.com>
2017-10-31 20:29:07 +0545

    Maintanence from Kanxi...

--- app/src/main/java/projectetaxi/etaxi_v1/NearByDriverActivity.java
+++ app/src/main/java/projectetaxi/etaxi_v1/NearByDriverActivity.java
@@ -9,5 +9,6 @@ public class NearByDriverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near_by_driver);
+        //change
     }
 }
